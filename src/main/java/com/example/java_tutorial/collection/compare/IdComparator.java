36a7fbb88d49f569697d8703ebefb38aa3217438
Minseo Kim <kms12782@gmail.com>
2025-01-29 20:51:18 +0900

    comparable, comparator

---
+++ src/main/java/com/example/java_tutorial/collection/compare/IdComparator.java
@@ -0,0 +1,10 @@
+package com.example.java_tutorial.collection.compare;
+
+import java.util.Comparator;
+
+public class IdComparator implements Comparator<MyUser> {
+  @Override
+  public int compare(final MyUser o1, final MyUser o2) {
+    return o1.getId().compareTo(o2.getId());
+  }
+}
