443b18abe4e70634c97e42cf997192b2634db996
Minseo Kim <kms12782@gmail.com>
2025-02-25 18:34:39 +0900

    생산자 소비자 문제 - Lock, ReentrantLock, Condition

--- src/main/java/com/example/java_adv1/thread/bounded/BoundedQueue.java
+++ src/main/java/com/example/java_adv1/thread/bounded/BoundedQueue.java
@@ -2,7 +2,7 @@ package com.example.java_adv1.thread.bounded;
 
 public interface BoundedQueue {
 
-  void put(String data) throws InterruptedException;
+  void put(String data);
 
   String take();
 
