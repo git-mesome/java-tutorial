11428b9a27035dc43eebbecbf0d6b8bdc955c7aa
Minseo Kim <kms12782@gmail.com>
2025-03-02 18:20:13 +0900

    ArrayBlockingQueue

--- src/main/java/com/example/java_adv1/thread/bounded/ProducerTask.java
+++ src/main/java/com/example/java_adv1/thread/bounded/ProducerTask.java
@@ -1,13 +1,14 @@
 package com.example.java_adv1.thread.bounded;
 
 import static com.example.java_adv1.util.MyLogger.log;
+import java.util.concurrent.BlockingQueue;
 
 public class ProducerTask implements Runnable {
 
-  private BoundedQueue queue;
-  private String request;
+  private final BlockingQueue<String> queue;
+  private final String request;
 
-  public ProducerTask(BoundedQueue queue, String request) {
+  public ProducerTask(BlockingQueue<String> queue, String request) {
     this.queue = queue;
     this.request = request;
   }
@@ -15,7 +16,19 @@ public class ProducerTask implements Runnable {
   @Override
   public void run() {
     log("[생산 시도] " + request + " -> " + queue);
-    queue.put(request);
+    // 1. 대기
+//    try {
+//      queue.put(request);
+//    } catch (InterruptedException e) {
+//      throw new RuntimeException(e);
+//    }
+
+    // 2. 대기시 반환
+//    boolean result = queue.offer(request);
+//    log("저장 시도 결과 = " + result);
+
+    // 3. 예외 반환
+    queue.add(request);
     log("[생산 완료] " + request + " -> " + queue);
   }
 }
