62513afb5a0f9af02f5a63d189cc836918b00a59
Minseo Kim <kms12782@gmail.com>
2025-03-10 19:44:57 +0900

    Reader, Writer

---
+++ src/main/java/com/example/java_adv2/io/text/ReaderWriterMainV4.java
@@ -0,0 +1,40 @@
+package com.example.java_adv2.io.text;
+
+import static com.example.java_adv2.io.text.TextConst.FILE_NAME;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReaderWriterMainV4 {
+
+  private static final int BUFFER_SIZE = 8912;
+
+  public static void main(String[] args) throws IOException {
+    String writeString = "ABC\n가나다";
+    System.out.println("=== Write String ===");
+    System.out.println(writeString);
+
+    FileWriter fw = new FileWriter(FILE_NAME); // 기본 인코딩 셋 사용 : UTF-8(for mac)
+    BufferedWriter bw = new BufferedWriter(fw, BUFFER_SIZE);
+    bw.write(writeString);
+    bw.close();
+
+    StringBuilder content = new StringBuilder();
+    FileReader fr = new FileReader(FILE_NAME, UTF_8);
+    BufferedReader br = new BufferedReader(fr, BUFFER_SIZE);
+
+    String line;
+    while ((line = br.readLine()) != null) {
+      content.append(line).append("\n");
+    }
+    br.close();
+
+    System.out.println("=== Read String ===");
+    System.out.println(content);
+  }
+
+}
